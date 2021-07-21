89acc3bd114f62fd9e6d4b1b21711a3e93c50adc
Rinshu14 <rajputrinshu@gmail.com>
2021-07-21 14:14:25 +0530

    menuoption

--- stylescript.js
+++ stylescript.js
@@ -1,67 +1,84 @@
-let allAlignmentOption=document.querySelectorAll(".align-cell-content span");
-let allColorOption=document.querySelectorAll(".cell-color-options span")
-let body=document.querySelector("body");
+let allAlignmentOption = document.querySelectorAll(".align-cell-content span");
+let allColorOption = document.querySelectorAll(".cell-color-options span")
+let body = document.querySelector("body");
+let file = document.querySelector(".file");
 
-let leftAlign=allAlignmentOption[0];
-let centerAlign=allAlignmentOption[1];
-let rightAlign=allAlignmentOption[2];
- 
-let bgColorPicker=allColorOption[0];
-let fontColorPicker=allColorOption[1]
+let leftAlign = allAlignmentOption[0];
+let centerAlign = allAlignmentOption[1];
+let rightAlign = allAlignmentOption[2];
 
-leftAlign.addEventListener("click",function(){
-    if(lastCell)
-    {
-       
-        lastCell.style.textAlign="left";
-        let Address=lastCell.getAttribute("data-address");
-        dataObj[Address].align="left";
+let bgColorPicker = allColorOption[0];
+let fontColorPicker = allColorOption[1];
+
+leftAlign.addEventListener("click", function () {
+    if (lastCell) {
+
+        lastCell.style.textAlign = "left";
+        let Address = lastCell.getAttribute("data-address");
+        dataObj[Address].align = "left";
     }
 })
-centerAlign.addEventListener("click",function(){
-    if(lastCell)
-    {
-        lastCell.style.textAlign="center";
-        let Address=lastCell.getAttribute("data-address");
-        dataObj[Address].align="center";
+centerAlign.addEventListener("click", function () {
+    if (lastCell) {
+        lastCell.style.textAlign = "center";
+        let Address = lastCell.getAttribute("data-address");
+        dataObj[Address].align = "center";
     }
 })
-rightAlign.addEventListener("click",function(){
-    if(lastCell)
-    {
-        lastCell.style.textAlign="right";
-        let Address=lastCell.getAttribute("data-address");
-        dataObj[Address].align="right";
+rightAlign.addEventListener("click", function () {
+    if (lastCell) {
+        lastCell.style.textAlign = "right";
+        let Address = lastCell.getAttribute("data-address");
+        dataObj[Address].align = "right";
     }
 })
 
- bgColorPicker.addEventListener("click",function(){
-     let coloPickerElement=document.createElement("input");
-     centerAlign.type="color";
-     body.append(coloPickerElement);
-     coloPickerElement.click();
+bgColorPicker.addEventListener("click", function () {
+    let coloPickerElement = document.createElement("input");
+    coloPickerElement.type = "color";
 
-     coloPickerElement.addEventListener("input",function(e){
+    body.append(coloPickerElement);
+    coloPickerElement.click();
+
+    coloPickerElement.addEventListener("input", function (e) {
         if (lastCell) {
             lastCell.style.backgroundColor = e.currentTarget.value;
             let address = lastCell.getAttribute("data-address");
             dataObj[address].bgColor = e.currentTarget.value;
-          }
-     })
+        }
+    })
 
- })
- fontColorPicker.addEventListener("click", function () {
+})
+fontColorPicker.addEventListener("click", function () {
     let colorPickerElement = document.createElement("input");
     colorPickerElement.type = "color";
     body.append(colorPickerElement);
     colorPickerElement.click();
 
     colorPickerElement.addEventListener("input", function (e) {
-      console.log(e.currentTarget.value);
-      if (lastCell) {
-        lastCell.style.color = e.currentTarget.value;
-        let address = lastCell.getAttribute("data-address");
-        dataObj[address].color = e.currentTarget.value;
-      }
+        console.log(e.currentTarget.value);
+        if (lastCell) {
+            lastCell.style.color = e.currentTarget.value;
+            let address = lastCell.getAttribute("data-address");
+            dataObj[address].color = e.currentTarget.value;
+        }
     });
-  });
+});
+file.addEventListener("click", function () {
+    let isopen = file.getAttribute("isopen")
+    
+    if (isopen == "false") {
+        
+        let dropdown = document.createElement("div");
+        dropdown.innerHTML = `<div>SAve</div>
+        <div>clear</div>`;
+        dropdown.classList.add("file-dropdown");
+        file.append(dropdown);
+        file.setAttribute("isopen", "true");
+    }
+    else {
+        document.querySelector(".file-dropdown").remove();
+        file.setAttribute("isopen", "false");
+    }
+
+})
